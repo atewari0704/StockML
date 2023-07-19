d6edf0c87f23fc34f3b90182d7ca3450d13201f1
Aditya Tewari <atewari3@wisc.edu>
2023-07-19 16:54:31 -0500

    bug fixes

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 import requests
-import json
+import json 
+
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
